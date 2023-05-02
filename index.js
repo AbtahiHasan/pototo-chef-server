2f7b32f779c166a104cabf0cd4820dde4ac1cc36
AbtahiHasan <anikidpersonal@gmail.com>
2023-05-02 10:22:25 +0600

    cors problem fixed

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express")
 const path = require("path")
 const app = express()
 const chefs = require("./database/chef.json")
+const cors = require("cors")
 
 const port = process.env.PORT || 3000
 const publicPath = path.join(__dirname, "./public")
-
+app.use(cors())
 app.use(express.static(publicPath))
 
 app.get("/", (req, res) => {
