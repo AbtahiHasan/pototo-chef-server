fe5220ac291c71d436243a92876c4f6c07207c20
AbtahiHasan <anikidpersonal@gmail.com>
2023-05-02 09:53:47 +0600

    chef json file added

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express")
+const path = require("path")
 const app = express()
 
 const port = process.env.PORT || 3000
+const publicPath = path.join(__dirname, "./public")
+console.log(publicPath)
+app.use(express.static())
 
 app.get("/", (req, res) => {
     res.send("Server is running")
